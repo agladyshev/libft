137658fd1ffafbca7123e2c3166e227cac72ae6d
Alexey Gladyshev <aglad@pm.me>
2021-04-26 14:53:50 +0300

    Add 2 bonus functions

---
+++ ft_lstnew.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stiffiny <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/03 13:25:19 by stiffiny          #+#    #+#             */
+/*   Updated: 2021/04/26 14:23:34 by stiffiny         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*new;
+
+	new = malloc(sizeof(t_list));
+	if (new)
+	{
+		new->next = 0;
+		new->content = content;
+	}
+	return (new);
+}
